8808501f10a0ae3bce75abd0a5620473c42e12d2
brkyzc-uk <burak.yazici@anadolu.edu.tr>
2022-05-09 14:40:42 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Burak YAZICI on 09/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
